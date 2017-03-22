e7cc304ade69574e75032529267e575fa4822a78
Niek Mes <Niek Mes>
2017-03-22 10:02:09 +0100

    fixed sidebar and footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
     <!-- footer content -->
     <footer>
         <div class="pull-right">
-            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+            HBO-ICT Patiënt Event Registratie
         </div>
         <div class="clearfix"></div>
     </footer>
