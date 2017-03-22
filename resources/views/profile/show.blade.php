4c50f70d94a50a99ab9b849433d861f06965f96d
Mahyar Emamjomeh <mahyar.e1996@gmail.com>
2017-03-22 11:20:14 +0100

    profiel pagina start1.1

---
+++ resources/views/profile/show.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.blank')
+
+@section('main_container')
+    <div class="right_col" role="main">
+        <div class="col-md-9">
+            <h1> {{$user->name}} 's Profiel</h1>
+        </div>
+    </div>
+@endsection
