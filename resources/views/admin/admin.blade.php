4f447e6b58f347ddda160ebe9cdaf1386e1429c4
Tim Roelse <Tim Roelse>
2017-03-22 14:52:34 +0100

    (admin) oops even verbeterd

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,92 +1,45 @@
-<?php
+@extends('layouts.blank')
 
-namespace App\Http\Controllers;
-use App\Models\User;
-use Auth;
-use App\Http\Requests\CreateUserFormRequest;
-
-class UserController extends Controller
-{
-
-    public function show($id)
-    {
-        return view('profile.show', [
-            'user' => User::findOrFail($id),
-        ]);
-    }
-
-    public function index()
-    {
-        return view('admin/admin', [
-            'users' => User::orderBy('id', 'asc')->get(),
-        ]);
-    }
-
-    public function store(CreateUserFormRequest $request)
-    {
-        $user = new User;
-
-        $user->first_name = $request->get('first_name');
-        $user->last_name = $request->get('last_name');
-        $user->function = $request->get('function');
-        $user->date_of_birth = $request->get('date_of_birth');
-        $user->postal_code = $request->get('postal_code');
-        $user->housenumber = $request->get('housenumber');
-        $user->street = $request->get('street');
-        $user->residence = $request->get('residence');
-        $user->email = $request->get('email');
-        $user->phone_number = $request->get('phone_number');
-        $user->username = $request->get('username');
-        $user->password = bcrypt($request->get('password'));
-        $user->password2 = bcrypt($request->get('password2'));
-
-        $user->save();
-
-        $request->session()->flash('alert-success', 'Gebruiker toegevoegd.');
-        return redirect('/admin');
-
-    }
-
-    public function edit($id)
-    {
-
-        $user = User::find($id);
+@section('main_container')
+    <div class="right_col" role="main">
+        <div class="col-md-9">
+            <a href="createuser">
+                <button type="button" class="btn btn-primary">
+                    Gebruiker Aanmaken
+                </button>
+            </a>
+            <div class="flash-message">
+                @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+                    @if(Session::has('alert-' . $msg))
+                        <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }}
+                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
+                    @endif
+                @endforeach
+            </div>
 
 
-        return View('admin/edit')
-            ->with('user', $user);
-    }
 
-    public function update($id)
-    {
-        // validate
-        // read more on validation at http://laravel.com/docs/validation
-        $rules = array(
-            'name'       => 'required',
-            'email'      => 'required|email',
-        );
-        $validator = Validator::make(Input::all(), $rules);
-
-
-        // store
-        $user = User::find($id);
-        $user->name       = Input::get('name');
-        $user->email      = Input::get('email');
-        $user->save();
-
-        // redirect
-        Session::flash('message', 'Successfully updated user!');
-        return Redirect::to('admin');
-    }
-    public function destroy($id)
-    {
-        // delete
-        $user = User::find($id);
-        $user->delete();
-
-        // redirect
-        Session::flash('message', 'Successfully deleted the User!');
-        return Redirect::to('admin');
-    }
-}
-
+            <table class="table table-striped table-hover">
+                <thead>
+                <th class="col-sm-1">Voornaam</th>
+                <th class="col-sm-1">Achternaam</th>
+                <th class="col-sm-1">Functie</th>
+                <th class="col-sm-1">Telefoonnumer</th>
+                <th class="col-sm-1">E-mailadres</th>
+                </thead>
+                <tbody>
+                @foreach ($users as $user)
+                    <tr class="row-link" style="cursor: pointer;"
+                        data-href="{{action('UserController@edit', ['id' => $user->id]) }}">
+                        <td class="table-text">{{ $user->first_name}}</td>
+                        <td class="table-text">{{ $user->last_name}}</td>
+                        <td class="table-text">{{ $user->function}}</td>
+                        <td class="table-text">{{ $user->phone_number}}</td>
+                        <td class="table-text">{{ $user->email}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
